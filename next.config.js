5efa8c8105e906d43d94445cd66feea6efe4560f
Muhammad Saqib <122340067+saaqib-v7@users.noreply.github.com>
2025-06-01 21:35:40 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio', // Replace with your repository name
+  assetPrefix: '/portfolio/', // Replace with your repository name
 }
 
 module.exports = nextConfig 
